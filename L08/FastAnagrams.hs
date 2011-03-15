9de470775ddbdb2b86cd62cab0fc094a9bb494be
Steven Shaw <steven@steshaw.org>
2011-03-15 18:15:48 +1000

    L08. Ungolfed implementation of fastAnagrams. Fixed bug by only using unique permutations

--- L08/FastAnagrams.hs
+++ L08/FastAnagrams.hs
@@ -5,14 +5,19 @@ import Data.List
 --import Data.Function
 import qualified Data.Set as S
 
+import Steshaw ((|>))
+
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
-fastAnagrams ::
-  String
-  -> FilePath
-  -> IO [String]
-fastAnagrams name f =
-  (flip (filter . flip S.member) (permutations name) . S.fromList . lines) `fmap` readFile f
+fastAnagrams :: String -> FilePath -> IO [String]
+fastAnagrams word f = do
+    contents <- readFile f
+    return $ frobnicate contents
+  where
+    frobnicate dictionary = findEm (makeSet dictionary)
+    findEm wordSet = filter ((flip S.member) wordSet) uniquePermutations
+    makeSet dictionary = lines dictionary |> S.fromList
+    uniquePermutations = permutations word |> S.fromList |> S.toList
 
 {-
 newtype NoCaseString =
